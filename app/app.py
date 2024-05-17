507f228fa302e0b2270e209ce5def2d116b74614
Maksym Pervov <s96523@pollub.edu.pl>
2024-05-17 17:05:42 +0200

    Updated files, adeed Dockerfile, Docker Compose and MySQL database file

---
+++ app/app.py
@@ -0,0 +1,107 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+from simple_analytics import *
+
+st.set_page_config(
+    page_title="CO\u2082 & N\u2082O Emissions EDA",
+    page_icon="🏭",
+    initial_sidebar_state="auto",
+)
+
+st.title("CO\u2082 & N\u2082O Emissions EDA ")
+
+data_type = st.sidebar.radio('Data',
+                             ('CO\u2082 emisiions per capita','N\u2082O emisiions per capita'))
+
+if data_type == 'CO\u2082 emisiions per capita':
+    df = pd.read_csv('./datasets/co-emissions-per-capita.csv')
+else: 
+    df = pd.read_csv('./datasets/per-capita-nitrous-oxide.csv')
+
+option = st.sidebar.selectbox(
+    'Select the tab',
+    ('Dataset Overview', 'Dataset Information', 'Graphs', 'ML Modeling'))
+
+if option == 'Dataset Overview':
+    st.title("Dataset Overview")
+    
+    st.dataframe(
+        df,
+        hide_index=True,
+        use_container_width=True
+    )
+
+elif option == 'Dataset Information':
+    st.title("Dataset Information")
+    
+    st.subheader("Data types of the columns:")
+    st.dataframe(df.dtypes)
+
+    st.subheader("Basic statistical information:")
+    st.dataframe(df.describe())
+
+elif option == 'Graphs':
+    st.title("Graphs")
+    
+    graph_option = st.sidebar.radio(
+        'Data type',
+        ('Emission Worldwide', 'Annual Emission by Country'))
+    
+    if graph_option == 'Emission Worldwide':
+
+        # Grouping the data by year and summing the values
+        total_emissions_year = df.groupby('Year')['Value'].sum()
+
+        fig = px.line(x=total_emissions_year.index, y=total_emissions_year.values)
+        fig.update_layout(
+            title='Annual Emissions by Year',
+            xaxis_title='Year',
+            yaxis_title='Total Emissions (mt)'
+        )
+        st.plotly_chart(fig)
+
+        # Grouping data by country and calculating total emissions
+        top_countries_emissions = df.groupby('Entity')['Value'].sum().nlargest(10)
+
+        fig = px.bar(x=top_countries_emissions.values, y=top_countries_emissions.index, orientation='h')
+        fig.update_layout(
+            title='Top 10 Countries with Highest Emissions',
+            xaxis_title='Annual Emissions (mt)',
+            yaxis_title='Country',
+        )
+        st.plotly_chart(fig)
+
+    elif graph_option == 'Annual Emission by Country':
+        # Filtering data by selected country
+        countries = df['Entity'].unique()
+
+        selected_country = st.selectbox('Select a country', countries)
+        country_data = df[df['Entity'] == selected_country]
+        fig = px.line(country_data, x='Year', y='Value')
+        fig.update_layout(
+            title='Emissions by Country',
+            xaxis_title='Year',
+            yaxis_title='Total Emissions (mt)'
+        )
+        st.plotly_chart(fig)
+
+elif option == 'ML Modeling':
+    st.title("ML Modeling")
+
+    # Creating an instance of EmissionDataModeling class
+    modeling = EmissionDataModeling(data_type)
+
+    # Training of models for all countries
+    modeling.fit_models()
+
+    # Country selection and number of forecast steps
+    selected_country = st.selectbox('Select a country', modeling.countries)
+    n_steps = st.slider('Number of steps to forecast', min_value=1, max_value=100, value=1)
+
+    # Plotting the forecast graph for the selected country
+    fig = modeling.plot_forecast(selected_country, n_steps)
+    st.plotly_chart(fig)
---
+++ app/simple_analytics.py
@@ -0,0 +1,102 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from statsmodels.tsa.arima.model import ARIMA
+
+# Auxiliary functions
+def create_model(emission_series):
+    if len(emission_series) == 0:
+        return None
+
+    # Convert index to datetime format
+    emission_series.index = pd.to_datetime(emission_series.index, format='%Y')
+
+    t = emission_series.index.map(lambda x: x.timestamp()).to_numpy()
+    t -= t[0]
+    y = emission_series.to_numpy()
+
+    model = ARIMA(y, order=(3, 1, 1))
+    model_fit = model.fit()
+
+    return model_fit
+
+
+def plot_time_series(emission_series, forecast, title):
+
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(
+            x=emission_series.index, y=emission_series, name="Observed", mode="lines+markers"
+        )
+    )
+    fig.add_trace(
+        go.Scatter(x=forecast.index, y=forecast, name="Forecast", mode="markers")
+    )
+    fig.update_layout(title=title)
+    return fig
+
+class EmissionDataModeling:
+
+    def __init__(self, emission_type):
+
+        # Choose the data set based on the emission type
+        if emission_type == "CO\u2082 emisiions per capita":
+            data_file = './datasets/co-emissions-per-capita.csv'
+        elif emission_type == "N\u2082O emisiions per capita":
+            data_file = './datasets/per-capita-nitrous-oxide.csv'
+        else:
+            raise ValueError(f"Unknown emission type: {emission_type}")
+
+        # Load the data set
+        self.df = pd.read_csv(data_file)
+
+        # Get all countries from the model - the id is the pair of Entity and country_code
+        countries = self.df["Entity"].unique()
+
+        # Average every year for each country
+        emissions_averaged = {}
+
+        for i in range(len(countries)):
+            emissions_averaged[countries[i]] = (
+                self.df[self.df["Entity"] == countries[i]]
+                .groupby("Year")["Value"]
+                .mean()
+            )
+
+        self.emissions_averaged = emissions_averaged
+        self.countries = countries
+
+    def fit_models(self):
+        self.models = {}
+
+        for country in self.countries:
+            model = create_model(self.emissions_averaged[country])
+            if model is not None:
+                self.models[country] = model
+
+    def predict(self, Entity, n_steps):
+        model = self.models[Entity]
+
+        forecast = model.forecast(steps=n_steps)
+
+        new_index = pd.date_range(
+            start=self.emissions_averaged[Entity].index[-1], periods=n_steps, freq="Y"
+        )
+
+        forecast = pd.Series(forecast, index=new_index)
+
+        return forecast
+    
+    def plot_forecast(self, Entity, n_steps):
+        # Get a forecast
+        forecast = self.predict(Entity, n_steps)
+
+        # Get historical data
+        emission_series = self.emissions_averaged[Entity]
+
+        # Plot the historical data and the forecast
+        fig = plot_time_series(emission_series, forecast, f'Emissions Forecast for {Entity}')
+
+        return fig
